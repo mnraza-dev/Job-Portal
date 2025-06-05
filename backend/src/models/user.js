b746f5a9c0ce17a8a3e2143ff1c1f91c8879f4d2
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-05 21:58:01 +0530

    Updated database connection logic to improve stability and performance.

---
+++ backend/src/models/user.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, "Password must be at least 6characters long"],
+    },
+    role: {
+      type: String,
+      enum: ["student", "recruiter"],
+      default: "student",
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
