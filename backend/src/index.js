d7180668f7bac1963cd9dd4cae45216187e7cbc8
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-05 21:53:59 +0530

    Database connected 🚀

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
-
 const PORT = process.env.PORT || 3000;
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running at http://localhost:${PORT}`);
 });
